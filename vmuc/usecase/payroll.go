b05e53e186d943dbea624ebfaeb97c1111fcbc72
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-08 13:16:30 +0800

    feat: Implement PayrollUseCase with CRUD operations for payroll management

---
+++ vmuc/usecase/payroll.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"context"
+	"time"
+	"vmuc-fintech-backend-web-go/domain"
+)
+
+type payrollUseCase struct {
+	payrollRepository domain.PayrollRepository
+	contextTimeout     time.Duration
+}
+
+func NewPayrollUseCase(payroll domain.PayrollRepository, t time.Duration) domain.PayrollUseCase {
+	return &payrollUseCase{
+		payrollRepository: payroll,
+		contextTimeout:     t,
+	}
+}
+
+func (c *payrollUseCase) FetchPayrollByID(ctx context.Context, id uint) (*domain.Payroll, error) {
+	res, err := c.payrollRepository.RetrievePayrollByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *payrollUseCase) FetchPayrolls(ctx context.Context) ([]domain.Payroll, error) {
+	res, err := c.payrollRepository.RetrieveAllPayroll()
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *payrollUseCase) AddPayroll(ctx context.Context, req *domain.Payroll) (*domain.Payroll, error) {
+	res, err := c.payrollRepository.CreatePayroll(req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *payrollUseCase) EditPayroll(ctx context.Context, req *domain.Payroll) (*domain.Payroll, error) {
+	res, err := c.payrollRepository.UpdatePayroll(req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *payrollUseCase) DeletePayroll(ctx context.Context, id uint) error {
+	err := c.payrollRepository.DeletePayroll(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}

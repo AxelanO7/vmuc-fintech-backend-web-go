c8ba21cd4475aef43227bef21f5d22c1aa77545d
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-08 13:16:14 +0800

    feat: Implement PayrollHandler with CRUD operations for payroll management

---
+++ vmuc/delivery/payroll.go
@@ -0,0 +1,174 @@
+package delivery
+
+import (
+	"strconv"
+	"vmuc-fintech-backend-web-go/domain"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+)
+
+type PayrollHandler struct {
+	PayrollUC domain.PayrollUseCase
+}
+
+func NewPayrollHandler(c *fiber.App, das domain.PayrollUseCase) {
+	handler := &PayrollHandler{
+		PayrollUC: das,
+	}
+	api := c.Group("/payroll")
+
+	_ = api.Group("/public")
+
+	private := api.Group("/private")
+	private.Get("/employee", handler.GetAllPayroll)
+	private.Get("/employee/:id", handler.GetPayrollByID)
+	private.Post("/employee", handler.CreatePayroll)
+	private.Put("/employee/:id", handler.UpdatePayroll)
+	private.Delete("/employee/:id", handler.DeletePayroll)
+}
+
+func (t *PayrollHandler) GetAllPayroll(c *fiber.Ctx) error {
+	res, err := t.PayrollUC.FetchPayrolls(c.Context())
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": err,
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  200,
+		"success": true,
+		"data":    res,
+		"message": "Successfully get all payroll",
+	})
+}
+
+func (t *PayrollHandler) GetPayrollByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	strId, erStr := strconv.Atoi(id)
+	if erStr != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse id",
+			"error":   erStr.Error(),
+		})
+	}
+	res, err := t.PayrollUC.FetchPayrollByID(c.Context(), uint(strId))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": err,
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  200,
+		"success": true,
+		"data":    res,
+		"message": "Successfully get payroll by id",
+	})
+}
+
+func (t *PayrollHandler) CreatePayroll(c *fiber.Ctx) error {
+	req := new(domain.Payroll)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse body",
+			"error":   err,
+		})
+	}
+	valRes, er := govalidator.ValidateStruct(req)
+	if !valRes {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse body",
+			"error":   er.Error(),
+		})
+	}
+	res, err := t.PayrollUC.AddPayroll(c.Context(), req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": err,
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"status":  201,
+		"success": true,
+		"data":    res,
+		"message": "Successfully create payroll",
+	})
+}
+
+func (t *PayrollHandler) UpdatePayroll(c *fiber.Ctx) error {
+	req := new(domain.Payroll)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse body",
+			"error":   err,
+		})
+	}
+	valRes, er := govalidator.ValidateStruct(req)
+	if !valRes {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse body",
+			"error":   er.Error(),
+		})
+	}
+	res, err := t.PayrollUC.EditPayroll(c.Context(), req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": err,
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  200,
+		"success": true,
+		"data":    res,
+		"message": "Successfully update payroll",
+	})
+}
+
+func (t *PayrollHandler) DeletePayroll(c *fiber.Ctx) error {
+	id := c.Params("id")
+	strId, erStr := strconv.Atoi(id)
+	if erStr != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": "Failed to parse id",
+			"error":   erStr.Error(),
+		})
+	}
+	err := t.PayrollUC.DeletePayroll(c.Context(), uint(strId))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  500,
+			"success": false,
+			"message": err,
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  200,
+		"success": true,
+		"message": "Successfully delete payroll",
+	})
+}

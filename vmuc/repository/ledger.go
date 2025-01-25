c6d1fcd39bb392eb1b2476f2a2ff0a6e5d805435
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-25 19:01:56 +0800

    Rename GeneralLedgerRepository to LedgerRepository and update related methods

--- vmuc/repository/ledger.go
+++ vmuc/repository/ledger.go
@@ -8,110 +8,110 @@ import (
 	"gorm.io/gorm"
 )
 
-type posgreGeneralLedgerRepository struct {
+type posgreLedgerRepository struct {
 	DB *gorm.DB
 }
 
-func NewPostgreGeneralLedger(client *gorm.DB) domain.GeneralLedgerRepository {
-	return &posgreGeneralLedgerRepository{
+func NewPostgreLedger(client *gorm.DB) domain.LedgerRepository {
+	return &posgreLedgerRepository{
 		DB: client,
 	}
 }
 
-func (a *posgreGeneralLedgerRepository) RetrieveAllGeneralLedger() ([]domain.GeneralLedger, error) {
-	var res []domain.GeneralLedger
+func (a *posgreLedgerRepository) RetrieveLedgers() ([]domain.Ledger, error) {
+	var res []domain.Ledger
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Find(&res).Error
 	if err != nil {
-		return []domain.GeneralLedger{}, err
+		return []domain.Ledger{}, err
 	}
 	fmt.Println("retrieve all GeneralLedger ", res)
 	return res, nil
 }
 
-func (a *posgreGeneralLedgerRepository) RetrieveGeneralLedgerByID(id uint) (*domain.GeneralLedger, error) {
-	var res domain.GeneralLedger
+func (a *posgreLedgerRepository) RetrieveLedgerByID(id uint) (*domain.Ledger, error) {
+	var res domain.Ledger
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Where("id = ?", id).
 		Take(&res).Error
 	if err != nil {
-		return &domain.GeneralLedger{}, err
+		return &domain.Ledger{}, err
 	}
 	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return &domain.GeneralLedger{}, fmt.Errorf("record not found")
+		return &domain.Ledger{}, fmt.Errorf("record not found")
 	}
 	fmt.Println("retrieve GeneralLedger by id ", res)
 	return &res, nil
 }
 
-func (a *posgreGeneralLedgerRepository) GetGeneralLedgerByGeneralLedgerPeriodeId(id uint) ([]domain.GeneralLedger, error) {
-	var res []domain.GeneralLedger
+func (a *posgreLedgerRepository) GetLedgerByPeriodeId(id uint) ([]domain.Ledger, error) {
+	var res []domain.Ledger
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Where("id_periode = ?", id).
 		Find(&res).Error
 	if err != nil {
-		return []domain.GeneralLedger{}, err
+		return []domain.Ledger{}, err
 	}
 	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return []domain.GeneralLedger{}, fmt.Errorf("record not found")
+		return []domain.Ledger{}, fmt.Errorf("record not found")
 	}
 	fmt.Println("retrieve GeneralLedger by id ", res)
 	return res, nil
 }
 
-func (a *posgreGeneralLedgerRepository) CreateGeneralLedger(GeneralLedger *domain.GeneralLedger) (*domain.GeneralLedger, error) {
+func (a *posgreLedgerRepository) CreateLedger(GeneralLedger *domain.Ledger) (*domain.Ledger, error) {
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Create(GeneralLedger).Error
 	if err != nil {
-		return &domain.GeneralLedger{}, err
+		return &domain.Ledger{}, err
 	}
 	fmt.Println("create GeneralLedger ", GeneralLedger)
 	return GeneralLedger, nil
 }
 
-func (a *posgreGeneralLedgerRepository) CreateBulkGeneralLedger(GeneralLedgers []*domain.GeneralLedger) ([]*domain.GeneralLedger, error) {
+func (a *posgreLedgerRepository) CreateBulkLedger(GeneralLedgers []*domain.Ledger) ([]*domain.Ledger, error) {
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Create(&GeneralLedgers).Error
 	if err != nil {
-		return []*domain.GeneralLedger{}, err
+		return []*domain.Ledger{}, err
 	}
 	fmt.Println("create bulk GeneralLedger ", GeneralLedgers)
 	return GeneralLedgers, nil
 }
 
-func (a *posgreGeneralLedgerRepository) UpdateGeneralLedger(GeneralLedger *domain.GeneralLedger) (*domain.GeneralLedger, error) {
+func (a *posgreLedgerRepository) UpdateLedger(GeneralLedger *domain.Ledger) (*domain.Ledger, error) {
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Where("id = ?", GeneralLedger.ID).
 		Updates(GeneralLedger).Error
 	if err != nil {
-		return &domain.GeneralLedger{}, err
+		return &domain.Ledger{}, err
 	}
 	fmt.Println("update GeneralLedger ", GeneralLedger)
 	return GeneralLedger, nil
 }
 
-func (a *posgreGeneralLedgerRepository) UpdateBulkGeneralLedger(GeneralLedgers []*domain.GeneralLedger) ([]*domain.GeneralLedger, error) {
+func (a *posgreLedgerRepository) UpdateBulkLedger(GeneralLedgers []*domain.Ledger) ([]*domain.Ledger, error) {
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Updates(&GeneralLedgers).Error
 	if err != nil {
-		return []*domain.GeneralLedger{}, err
+		return []*domain.Ledger{}, err
 	}
 	fmt.Println("update bulk GeneralLedger ", GeneralLedgers)
 	return GeneralLedgers, nil
 }
 
-func (a *posgreGeneralLedgerRepository) DeleteGeneralLedger(id uint) error {
+func (a *posgreLedgerRepository) DeleteLedger(id uint) error {
 	err := a.DB.
-		Model(domain.GeneralLedger{}).
+		Model(domain.Ledger{}).
 		Where("id = ?", id).
-		Delete(&domain.GeneralLedger{}).Error
+		Delete(&domain.Ledger{}).Error
 	if err != nil {
 		return err
 	}
